99f31eb3c7920d7375bfa1ddd3edf0a6751adf1d
Chocolateandmilkwin <nicknielsen1995@hotmail.com>
2023-10-22 10:21:59 +0700

    test1

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,16 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[get("/test")]
+async fn hello_world_1() -> &'static str {
+    "Hello World!"
+}
+
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.service(hello_world);
+        cfg.service(hello_world_1);
     };
 
     Ok(config.into())
